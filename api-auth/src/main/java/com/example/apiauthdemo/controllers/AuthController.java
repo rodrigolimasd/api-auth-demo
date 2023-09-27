be80633ff7e7e5c41cd92cfcc8ede32ac90a6b32
Rodrigo Lima (Smart) <rodrigo.lima@smart-system.net.br>
2023-09-27 19:07:21 -0300

    jwt

---
+++ api-auth/src/main/java/com/example/apiauthdemo/controllers/AuthController.java
@@ -0,0 +1,43 @@
+package com.example.apiauthdemo.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+import java.util.stream.Collectors;
+
+@RestController
+public class AuthController {
+
+    private final JwtEncoder jwtEncoder;
+
+    public AuthController(JwtEncoder jwtEncoder) {
+        this.jwtEncoder = jwtEncoder;
+    }
+
+    @PostMapping("/v1/auth/token")
+    public ResponseEntity<String> token(Authentication authentication) {
+        Instant now = Instant.now();
+        long expiry = 36000L;
+        String scope = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(" "));
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuer("self")
+                .issuedAt(now)
+                .expiresAt(now.plusSeconds(expiry))
+                .subject(authentication.getName())
+                .claim("scope", scope)
+                .build();
+        var jwt = jwtEncoder.encode(JwtEncoderParameters.from(claims));
+        return ResponseEntity.ok(jwt.getTokenValue());
+    }
+
+
+}
--- api-auth/src/main/java/com/example/apiauthdemo/controllers/HomeController.java
+++ api-auth/src/main/java/com/example/apiauthdemo/controllers/HomeController.java
@@ -1,6 +1,7 @@
 package com.example.apiauthdemo.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
@@ -14,21 +15,21 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "Hello, home!";
+        return "Hello";
     }
 
     @GetMapping("/secured")
-    public String secured() {
-        return "Hello, secured";
+    public String secured(Authentication authentication) {
+        return "Hello "+ authentication.getName();
     }
 
-    @GetMapping("/get-token")
-    public ResponseEntity<OAuth2AccessToken> getAccessToken(@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient) {
-        return ResponseEntity.ok(authorizedClient.getAccessToken());
-    }
-
-    @GetMapping("/get-email")
-    public String getEmail(@AuthenticationPrincipal OAuth2User oauth2User) {
-        return oauth2User.getAttribute("email");
-    }
+//    @GetMapping("/get-token")
+//    public ResponseEntity<OAuth2AccessToken> getAccessToken(@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient) {
+//        return ResponseEntity.ok(authorizedClient.getAccessToken());
+//    }
+//
+//    @GetMapping("/get-email")
+//    public String getEmail(@AuthenticationPrincipal OAuth2User oauth2User) {
+//        return oauth2User.getAttribute("email");
+//    }
 }

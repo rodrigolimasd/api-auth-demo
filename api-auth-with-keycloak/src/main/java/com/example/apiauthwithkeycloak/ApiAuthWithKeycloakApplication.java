b25729a34fd6dc1f47394da4e2636dfecc3add00
Rodrigo Lima (Smart) <rodrigo.lima@smart-system.net.br>
2023-09-28 20:43:11 -0300

    add keycloak client

---
+++ api-auth-with-keycloak/src/main/java/com/example/apiauthwithkeycloak/ApiAuthWithKeycloakApplication.java
@@ -0,0 +1,13 @@
+package com.example.apiauthwithkeycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAuthWithKeycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAuthWithKeycloakApplication.class, args);
+	}
+
+}
---
+++ api-auth-with-keycloak/src/test/java/com/example/apiauthwithkeycloak/ApiAuthWithKeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiauthwithkeycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAuthWithKeycloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
